a7e62e84bf34e7082f96fdbb4eec04de3268b075
unknown <gpupro@outlook.com>
2013-06-23 23:19:17 -0300

    Adding cmake file. Fixes for msvc support. Currently 36 out of 43 demos can build with msvc.

---
+++ msvc/alg_vs.h
@@ -0,0 +1,18 @@
+#ifndef __ALGVS_H__
+#define __ALGVS_H__
+#ifdef _MSC_VER
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#define __func__ __FUNCTION__
+
+#define strtok_r strtok_s
+
+#if _MSC_VER <= 1700
+#define ALG_VLA_NOT_SUPPORTED 1
+#endif
+
+#define typeof decltype
+
+#endif//_MSC_VER
+#endif//__ALGVS_H__
---
+++ msvc/stdbool.h
@@ -0,0 +1,3 @@
+#ifdef _MSC_VER
+
+#endif
