a42f72f74dac4ddc58213f14b6b722429f161787
fuli <fuli@nibirutech.com>
2013-09-12 17:25:16 +0800

    update base64 for code formatting

--- src/base64_demo.cpp
+++ src/base64_demo.cpp
@@ -1,32 +1,19 @@
-
 #include <iostream>
 #include "base64.h"
 #include <string.h>
 
+using namespace std;
 
-
-int main()
-{
-	char *s="Man is distinguished not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure..";
+int main() {
+	const char *s="Man is distinguished not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure..";
 
 	cout << " INPUT: " << endl;
-	cout << s << endl;
-	cout << endl;
+	cout << s << endl << endl;
 
-	string base=CBase64::encodeBase64((unsigned char *) s, strlen(s));
-	cout << " encodeBase64: " <<endl;
-	cout << base << endl;
-	cout << endl;
-	cout << endl;
-
+	string base=alg::CBase64::encodeBase64((unsigned char *) s, strlen(s));
+	cout << " encodeBase64: " << endl;
+	cout << base << endl << endl;
 
 	cout << " decodeBase64: " << endl;
-	cout << CBase64::decodeBase64(base) <<endl;
-
-
-	
-	return 1;
+	cout << alg::CBase64::decodeBase64(base) << endl;
 }
-
-
-
