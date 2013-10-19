b17fed84bb87d61d155a91be191b22eca4b258ce
xtaci <daniel820313@gmail.com>
2013-10-19 08:39:52 +0800

    Revert "add debug info"
    This reverts commit 39b1dc20c983c8b9af4a5ee693ff5e1cd24b9954.

--- src/btree_demo.cpp
+++ src/btree_demo.cpp
@@ -6,9 +6,11 @@ int main(void) {
 	BTree x("./btree.dat");
 	int32_t i;
 
+	/*
 	for (i=0;i<1024;i++) {
-		x.Insert(i);
+		x.Insert(i);	
 	}
+	*/
 
 	for(i=0;i<1024;i++) {
 		int32_t r = x.Search(i);
