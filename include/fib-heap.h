bc6189505316161c5e4180ead69dc0c17941e58c
xtaci <daniel820313@gmail.com>
2013-10-30 15:47:18 +0800

    implementing fib-heap

--- include/fib-heap.h
+++ include/fib-heap.h
@@ -49,6 +49,9 @@ namespace alg {
 			INIT_LIST_HEAD(&rootlist);
 		}
 
+		/**
+		 * insert a value into the Fibonacci Heap
+		 */
 		void Insert(key_type key, value_type value) {
 			Node x = new node_t;
 			x->degree = 0;
@@ -65,6 +68,37 @@ namespace alg {
 				}
 			}
 			n = n+1;
+		}
+
+		/**
+		 * Union 2 Fibonacci-Heap
+		 */
+		static FibHeap* Union(FibHeap *H1, FibHeap *H2) {
+			FibHeap * H = new FibHeap();
+			H->min = H1->min;
+			H->rootlist = H1->rootlist;
+			list_splice(&H->rootlist, &H1->rootlist);	// concat 2 root-list
+			list_splice(&H->rootlist, &H2->rootlist);
+			if (H1->min == NULL || (H2.min != NULL && H2->min.key < H1->min.key)) {
+				H->min = H2->min;
+			}
+			H->n = H1->n + H2->n;
+			return H;
+		}
+
+		/**
+		 * Extract Min Element
+		 */
+		Node * ExtractMin() {
+			Node z = min;
+			if (z != NULL) {
+				Node n, ns;
+				// for each child x of z, add x to the root list of H
+				list_for_each_entry_safe(n,ns, z->child, node){
+					list_add(&n->node, &rootlist);
+					n->parent = NULL;
+				}
+			}
 		}
 	};
 }
