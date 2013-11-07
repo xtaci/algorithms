3aaa2d93c96553507af6b096e8075af8560b82fc
fuli <fuli@nibirutech.com>
2013-11-07 13:15:17 +0800

    seperate hash_code into different file

--- include/hash_table.h
+++ include/hash_table.h
@@ -22,43 +22,10 @@
 #include <limits.h>
 #include <stdexcept>
 #include "double_linked_list.h"
+#include "hash_code.h"
 #include "hash_multi.h"
-#include "hash_string.h"
 
 namespace alg {
-	/**
-	 * default compare funcs
-	 */
-	template<typename T> 
-	struct hash_code {
-		uint32_t operator()(T) {
-			printf("must provide a hash function for _Key\n");
-			return 0;
-		}
-	};
-
-	template<>
-	struct hash_code<const char *> {
-		uint32_t operator()(const char* value) {
-			return hash_fnv1a(value, strlen(value));
-		}
-	};
-
-	template<>
-	struct hash_code<uint32_t> {
-		uint32_t operator()(uint32_t value) {
-			return value;
-		}
-	};
-
-	template<>
-	struct hash_code<int32_t> {
-		uint32_t operator()(int32_t value) {
-			return (uint32_t)value;
-		}
-	};
-
-
 	/**
 	 * definition of a hash table.
 	 */
