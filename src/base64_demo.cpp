b7a6b216ac5234551610802c28af51833c03deac
xtaci <daniel820313@gmail.com>
2013-11-11 13:56:57 +0800

    update demo of base64

--- src/base64_demo.cpp
+++ src/base64_demo.cpp
@@ -5,8 +5,7 @@
 using namespace std;
 
 int main() {
-	const char *s="Man is distinguished not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure..";
-
+	const char *s = "Matthew 7:24-27 Therefore everyone who hears these words of mine and puts them into practice is like a wise man who built his house on the rock. 25 The rain came down, the streams rose, and the winds blew and beat against that house; yet it did not fall, because it had its foundation on the rock. 26 But everyone who hears these words of mine and does not put them into practice is like a foolish man who built his house on sand. 27 The rain came down, the streams rose, and the winds blew and beat against that house, and it fell with a great crash.";
 	cout << " INPUT: " << endl;
 	cout << s << endl << endl;
 
