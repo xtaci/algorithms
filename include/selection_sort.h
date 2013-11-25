2533f02e84a0b8d5995b201e2df5a4a6e1a23fb1
xtaci <daniel820313@gmail.com>
2013-11-25 18:37:42 +0800

    update selection sort

--- include/selection_sort.h
+++ include/selection_sort.h
@@ -1,46 +1,27 @@
-#ifndef _selection_sort_h_
-#define _selection_sort_h_
+#ifndef __SELECTION_SORT_H__
+#define __SELECTION_SORT_H__
 
-
-
-namespace aglo{
-	
+namespace alg {
 	template<typename T>
-	static void SelectionSort(T list[],int start,int end){
-	
-		int min_index;
-		T tmp;
+		static void SelectionSort(T list[],int start,int end){
+			int min_index;
+			T tmp;
+			if(start < end){
+				for(size_t i = start; i <= end-1; ++i) {
+					min_index=i;
+					for(size_t j = i+1; j <= end; ++j) {
+						if ( list[i] >= list[j])
+							min_index=j;
+					}
 
-		if(start < end){
-			
-			for(size_t i = start; i <= end-1; ++i)
-			{
-				min_index=i;
-				for(size_t j = i+1; j <= end; ++j)
-				{
-					if ( list[i] >= list[j])
-						min_index=j;
-			
-				}
-		
-				if (i != min_index)
-				{
-					tmp=list[i];
-					list[i]=list[min_index];
-					list[min_index]=tmp;
-			
+					if (i != min_index) {
+						tmp=list[i];
+						list[i]=list[min_index];
+						list[min_index]=tmp;
+					}
 				}
 			}
-			
 		}
-
-		
-	
-	}
-
 }
 
-
-
-
-#endif
+#endif //__SELECTION_SORT_H__
--- src/selection_sort_demo.cpp
+++ src/selection_sort_demo.cpp
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h> 
 #include <time.h>
@@ -6,10 +5,8 @@
 #include "generic.h"
 #include "selection_sort.h"
 
-
 template<typename T>
-static void printlist(T & list,int count)
-{
+static void printlist(T & list,int count) {
 	int i;
 	for(i=0;i<count;i++)
 		printf("%d\t ",list[i]);
@@ -17,8 +14,7 @@ static void printlist(T & list,int count)
 }
 
 
-int main (int argc, char const *argv[])
-{
+int main (int argc, char const *argv[]) {
 	const int MAX_ELEMENTS = 10;
 	int list[MAX_ELEMENTS];
 	for(int i = 0; i < MAX_ELEMENTS; i++ ){
@@ -27,11 +23,9 @@ int main (int argc, char const *argv[])
 	printf("The list before sorting is:\n");
 	printlist(list,MAX_ELEMENTS);
 
-	// sort the list using quicksort
-	aglo::SelectionSort(list,0,MAX_ELEMENTS-1);
+	alg::SelectionSort(list,0,MAX_ELEMENTS-1);
 
-	// print the result
-	printf("The list after sorting using quicksort algorithm:\n");
+	printf("The list after sorting using selection sort algorithm:\n");
 	printlist(list,MAX_ELEMENTS);
 	
 	return 0;
