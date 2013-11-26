eb35a77cd0b7a669954ada746dc41778649f8f8b
xtaci <daniel820313@gmail.com>
2013-11-26 10:11:55 +0800

    update selection sort

--- include/selection_sort.h
+++ include/selection_sort.h
@@ -1,24 +1,49 @@
+/*******************************************************************************
+ * ALGORITHM IMPLEMENTAIONS
+ *
+ *  /\  |  _   _  ._ o _|_ |_  ._ _   _ 
+ * /--\ | (_| (_) |  |  |_ | | | | | _> 
+ *         _|                      
+ *
+ * SELECTION SORT
+ *
+ * In computer science, selection sort is a sorting algorithm, specifically an 
+ * in-place comparison sort. It has O(n2) time complexity, making it inefficient
+ * on large lists, and generally performs worse than the similar insertion sort.
+ * Selection sort is noted for its simplicity, and it has performance advantages 
+ * over more complicated algorithms in certain situations, particularly where 
+ * auxiliary memory is limited.
+ *
+ * http://en.wikipedia.org/wiki/Selection_sort
+ ******************************************************************************/
+
 #ifndef __SELECTION_SORT_H__
 #define __SELECTION_SORT_H__
 
+#include <assert.h>
+#include <generic.h>
+
 namespace alg {
 	template<typename T>
-		static void SelectionSort(T list[],int start,int end){
-			int min_index;
-			T tmp;
-			if(start < end){
-				for(size_t i = start; i <= end-1; ++i) {
-					min_index=i;
-					for(size_t j = i+1; j <= end; ++j) {
-						if ( list[i] >= list[j])
-							min_index=j;
-					}
+		static void SelectionSort(T list[], int start, int end) {
+			int i,j;
+			int iMin;
 
-					if (i != min_index) {
-						tmp=list[i];
-						list[i]=list[min_index];
-						list[min_index]=tmp;
+			assert(start <= end);
+
+			for(j = start; j <= end-1; j++) {
+				// assume the min is the first element */
+				iMin = j;
+				// test against elements after i to find the smallest
+				for(i = j+1; i <= end; i++) {
+					if (list[i] < list[iMin]) {
+						iMin = i;
 					}
+				}
+
+				// iMin is the index of the minimum element. Swap it with the current position
+				if (iMin != j) {
+					swap(list[j], list[iMin]);
 				}
 			}
 		}
--- src/selection_sort_demo.cpp
+++ src/selection_sort_demo.cpp
@@ -1,8 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h> 
-#include <time.h>
 
-#include "generic.h"
 #include "selection_sort.h"
 
 template<typename T>
@@ -12,7 +9,6 @@ static void printlist(T & list,int count) {
 		printf("%d\t ",list[i]);
 	printf("\n");
 }
-
 
 int main (int argc, char const *argv[]) {
 	const int MAX_ELEMENTS = 10;
