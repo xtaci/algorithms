d5490398ab810141da155755ec5b5ae442b4241a
Ivan Lyon <sunshine1914@live.cn>
2014-02-06 16:19:46 +0800

    快速排序：枢纽值「pivot」选取进行优化
    https://github.com/xtaci/algorithms/issues/14

--- include/quick_sort.h
+++ include/quick_sort.h
@@ -19,31 +19,49 @@
 #define __QUICKSORT_H__
 
 #include <generic.h> 
+#include <cassert>
 
 namespace alg {
+
+	/**
+     * Return median of begin, middle, and end.
+	 * Order these and hide the pivot.
+     */
+	template<typename T>
+	static const T & __median3(T list[], int begin, int end) {
+		assert(begin + 2 <= end);
+		int middle = end - (end - begin) / 2;
+		if (list[middle] < list[begin])
+			swap(list[middle], list[begin]);
+		if (list[end] < list[begin])
+			swap(list[end], list[begin]);
+		if (list[end] < list[middle])
+			swap(list[end], list[middle]);
+	
+		//Place pivot at position [end - 1]
+		swap(list[middle], list[end - 1]);
+		return list[end - 1];
+	}
+	
 	/**
 	 * the quick-sort partition routine
 	 */
 	template<typename T>
 		static int __partition(T list[],int begin, int end) {
-			int pivot_idx = RANDOM(begin,end);
-			T pivot = list[pivot_idx];
-			swap(list[begin], list[pivot_idx]);
+			T pivot = __median3<T>(list, begin, end);
 
-			int i = begin + 1;
-			int j = end;
+			int i = begin;
+			int j = end - 1;
 
-			while(i <= j) {
-				while((i <= end) && (list[i] <= pivot))
-					i++;
-				while((j >= begin) && (list[j] > pivot))
-					j--;
+			while(i < j) {
+				while(list[++i] < pivot) {}
+				while(pivot < list[--j]) {}
 				if(i < j)
 					swap(list[i],list[j]);
 			}
 
-			swap(list[begin],list[j]);
-			return j; // final pivot position
+			swap(list[i],list[end - 1]);
+			return i; // final pivot position
 		}
 
 	/**
@@ -51,10 +69,13 @@ namespace alg {
 	 */
 	template<typename T>
 		static void quicksort(T list[],int begin,int end) {
-			if( begin < end) {
+			if( begin + 1 < end) {
 				int pivot_idx = __partition<T>(list, begin, end);
 				quicksort(list, begin, pivot_idx-1);
 				quicksort(list, pivot_idx+1, end);
+			} else if ( begin + 1 == end) {
+				if (list[begin + 1] > list[end])
+					swap(list[begin + 1], list[end]);
 			}
 		}
 }
