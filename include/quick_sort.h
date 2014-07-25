f1473fa85ee067efa35244c230aaac8bd291ddd5
xtaci <daniel820313@gmail.com>
2014-07-25 17:16:30 +0800

    rollback quicksort to random pivot version

--- include/quick_sort.h
+++ include/quick_sort.h
@@ -19,49 +19,31 @@
 #define __QUICKSORT_H__
 
 #include <generic.h> 
-#include <cassert>
 
 namespace alg {
-
-	/**
-     * Return median of begin, middle, and end.
-	 * Order these and hide the pivot.
-     */
-	template<typename T>
-	static const T & __median3(T list[], int begin, int end) {
-		assert(begin + 2 <= end);
-		int middle = end - (end - begin) / 2;
-		if (list[middle] < list[begin])
-			swap(list[middle], list[begin]);
-		if (list[end] < list[begin])
-			swap(list[end], list[begin]);
-		if (list[end] < list[middle])
-			swap(list[end], list[middle]);
-	
-		//Place pivot at position [end - 1]
-		swap(list[middle], list[end - 1]);
-		return list[end - 1];
-	}
-	
 	/**
 	 * the quick-sort partition routine
 	 */
 	template<typename T>
 		static int __partition(T list[],int begin, int end) {
-			T pivot = __median3<T>(list, begin, end);
+			int pivot_idx = RANDOM(begin,end);
+			T pivot = list[pivot_idx];
+			swap(list[begin], list[pivot_idx]);
 
-			int i = begin;
-			int j = end - 1;
+			int i = begin + 1;
+			int j = end;
 
-			while(i < j) {
-				while(list[++i] < pivot) {}
-				while(pivot < list[--j]) {}
+			while(i <= j) {
+				while((i <= end) && (list[i] <= pivot))
+					i++;
+				while((j >= begin) && (list[j] > pivot))
+					j--;
 				if(i < j)
 					swap(list[i],list[j]);
 			}
 
-			swap(list[i],list[end - 1]);
-			return i; // final pivot position
+			swap(list[begin],list[j]);
+			return j; // final pivot position
 		}
 
 	/**
@@ -69,13 +51,10 @@ namespace alg {
 	 */
 	template<typename T>
 		static void quicksort(T list[],int begin,int end) {
-			if( begin + 1 < end) {
+			if( begin < end) {
 				int pivot_idx = __partition<T>(list, begin, end);
 				quicksort(list, begin, pivot_idx-1);
 				quicksort(list, pivot_idx+1, end);
-			} else if ( begin + 1 == end) {
-				if (list[begin] > list[end])
-					swap(list[begin], list[end]);
 			}
 		}
 }
