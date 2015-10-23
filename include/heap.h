b5756134050f5aeab5b430674cc27ad1984ae88c
Daniel Fu <daniel820313@gmail.com>
2015-10-23 17:05:22 +0800

    Update heap.h

--- include/heap.h
+++ include/heap.h
@@ -7,14 +7,23 @@
  *
  * Heap Data structure
  *
- * Heaps can be used as an array. For any key at array position I,
- * left child is at ( 2i ), right child is at ( 2i+1 ) and parent is 
- * at (int) (i / 2). Heap size is stored at index 0.
+ * In computer science, a heap is a specialized tree-based data structure that 
+ * satisfies the heap property: If A is a parent node of B then the key of node
+ * A is ordered with respect to the key of node B with the same ordering applying 
+ * across the heap. Heaps can be classified further as either a "max heap" or 
+ * a "min heap". In a max heap, the keys of parent nodes are always greater
+ * than or equal to those of the children and the highest key is in the root node.
+ * In a min heap, the keys of parent nodes are less than or equal to those of 
+ * the children and the lowest key is in the root node. Heaps are crucial in 
+ * several efficient graph algorithms such as Dijkstra's algorithm, and in 
+ * the sorting algorithm heapsort. A common implementation of a heap is the 
+ * binary heap, in which the tree is a complete binary tree (see figure).
  *
  * Basic operations of a heap are:
  *
- * 1. Insert – Insert an key.
- * 2. Delete minimum – Delete and return the smallest item in the heap.
+ * 1. Push – Insert an key.
+ * 2. Pop – Delete and return the smallest item in the heap.
+ * 3. Remove - Remove an element
  *
  * http://en.wikipedia.org/wiki/Binary_heap
  ******************************************************************************/
