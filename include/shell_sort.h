6ca83667d1c7f39827b1ee0ca552548c503391ce
xtaci <daniel820313@gmail.com>
2016-03-31 09:47:50 +0800

    indent

--- include/shell_sort.h
+++ include/shell_sort.h
@@ -22,22 +22,22 @@ namespace alg {
 	 */
 	template<typename T>
 		static void shell_sort(T *array, int len) {
-      int h = 1;
-      while (h < len / 3) {
-        h = 3 * h + 1; // 1, 4, 13, 40, 121, ...
-      }
-      while (h >= 1) {
-        for (int i = h; i < len; i++) {
-          int cur = array[i];
-          int j = i - h;
-          while (j >= 0 && array[j] > cur) {
-            array[j + h] = array[j];
-            j = j - h;
-          }
-          array[j + h] = cur;
-        }
-        h = h / 3;
-      }
+			int h = 1;
+			while (h < len / 3) {
+				h = 3 * h + 1; // 1, 4, 13, 40, 121, ...
+			}
+			while (h >= 1) {
+				for (int i = h; i < len; i++) {
+					int cur = array[i];
+					int j = i - h;
+					while (j >= 0 && array[j] > cur) {
+						array[j + h] = array[j];
+						j = j - h;
+					}
+					array[j + h] = cur;
+				}
+				h = h / 3;
+			}
 		}
 }
 
