b8758ce5261b5a3230063aecbabfb25bb38ec905
AdUhTkJm <30948580+AdUhTkJm@users.noreply.github.com>
2019-08-22 20:20:33 +0800

    Adjusted indent

--- include/segment_tree.h
+++ include/segment_tree.h
@@ -26,13 +26,13 @@ namespace alg {
 		inline int r(int x) { return x << 1 | 1; }
 		void push_down(int b, int e, int x) {
 			int mid = b + e >> 1;
-		    a[l(x)] = a[l(x)] * mul[x] + plus[x] * (mid - b + 1);
-		    a[r(x)] = a[r(x)] * mul[x] + plus[x] * (e - mid);
-		    mul[l(x)] = mul[l(x)] * mul[x];
-		    mul[r(x)] = mul[r(x)] * mul[x];
+			a[l(x)] = a[l(x)] * mul[x] + plus[x] * (mid - b + 1);
+			a[r(x)] = a[r(x)] * mul[x] + plus[x] * (e - mid);
+			mul[l(x)] = mul[l(x)] * mul[x];
+			mul[r(x)] = mul[r(x)] * mul[x];
 			plus[l(x)] = plus[l(x)] * mul[x] + plus[x];
-		    plus[r(x)] = plus[r(x)] * mul[x] + plus[x];
-		    mul[x] = 1;
+			plus[r(x)] = plus[r(x)] * mul[x] + plus[x];
+			mul[x] = 1;
 			plus[x] = 0;
 		}
 	
@@ -42,18 +42,18 @@ namespace alg {
 		int build(int* source, int sz, int x = 1, int b = 1, int e = -1) {
 			if (e == -1)
 				siz = e = sz;
-		    mul[x] = 1;
-		    int mid = b + e >> 1;
+			mul[x] = 1;
+			int mid = b + e >> 1;
 			if (b == e)
-		    	return a[x] = source[b];
-		    else
-				return a[x] = build(source, sz, l(x), b, mid) + build(source, sz, r(x), mid + 1, e);
+				return a[x] = source[b];
+			else
+			return a[x] = build(source, sz, l(x), b, mid) + build(source, sz, r(x), mid + 1, e);
 		}
 		
 		void multiply(int b_, int e_, int d, int x = 1, int b = 1, int e = -1) {
 			if (e == -1)
 				e = siz;
-		    if (e_ < b || e < b_)
+			if (e_ < b || e < b_)
 				return;
 			if (b_ <= b && e <= e_) {
 				mul[x] *= d;
@@ -71,7 +71,7 @@ namespace alg {
 		void add(int b_, int e_, int d, int x = 1, int b = 1, int e = -1) {
 			if (e == -1)
 				e = siz;
-		    if (e_ < b || e < b_)
+			if (e_ < b || e < b_)
 				return;
 			if (b_ <= b && e <= e_) {
 				plus[x] += d;
@@ -88,7 +88,7 @@ namespace alg {
 		int query(int b_, int e_, int x = 1, int b = 1, int e = -1) {
 			if (e == -1)
 				e = siz;
-		    if (e_ < b || e < b_)
+			if (e_ < b || e < b_)
 				return 0;
 			if (b_ <= b && e <= e_)
 				return a[x];
