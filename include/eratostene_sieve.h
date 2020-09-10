e92a29dd6d893b899f359f1e91c26cd82c103e99
marius004 <scarlatmariusstefan2018@gmail.com>
2020-09-10 18:16:17 +0300

    Added Eratostene Sieve

---
+++ include/eratostene_sieve.h
@@ -0,0 +1,68 @@
+//
+// Created by Scarlat Marius on 9/10/20.
+//
+
+#ifndef ALGORITHM_ERATOSTENE_SIEVE_H
+#define ALGORITHM_ERATOSTENE_SIEVE_H
+
+#include <vector>
+#include <stdexcept>
+
+namespace alg {
+
+    /*
+     * A short implementation of Eratostene's Sieve
+     */
+
+    template < int SIZE >
+    class EratosteneSieve {
+
+    private:
+
+        vector < bool > prime;
+        vector < int > primeCache; // cache all the prime numbers when the build method is called
+        bool build_called; // if build() was called
+
+        EratosteneSieve() {
+            prime.resize(SIZE + 1, true);
+            build_called = false;
+        }
+
+        void build() {
+            this->build_called = true;
+            primeCache.emplace_back(2);
+
+            prime[0] = prime[1] = false; // false means not prime
+
+            // k * 2 is not prime, where k > 1
+            for(int i = 4;i * i <= n;i += 2)
+                prime[i] = false;
+
+            for(int i = 3;i * i <= n; i += 2) {
+                if (prime[i]) { // all the multiples of a prime number are not prime
+                    primeCache.emplace_back(i);
+
+                    for(int j = i + i;j <= n;j += i)
+                        prime[j] = false;
+                }
+            }
+        }
+
+        bool isPrime(const unsigned int& nr) {
+            if(nr > SIZE)
+                throw std::out_of_range("Index out of range");
+
+            if(!build_called)
+                throw "You need to call build() first";
+
+            return prime[nr];
+        }
+
+        vector < int > getPrimes() {
+            return this->primeCache;
+        }
+    };
+
+}
+
+#endif //ALGORITHM_ERATOSTENE_SIEVE_H
---
+++ src/eratostene_sieve.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Scarlat Marius on 9/10/20.
+//
+
+#include <iostream>
+#include <vector>
+
+#include "eratostene_sieve.h"
+
+using namespace std;
+using namespace alg;
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(false); cout.tie(false);
+
+    EratosteneSieve < 100 > sieve;
+    sieve.build();
+
+    vector < int > prime = sieve.getPrimes();
+
+    cout << "PRIME NUMBERS UNTIL 100\n";
+
+    for(auto it : prime)
+        cout << it << ' ';
+
+    int x;
+    cin >> x;
+
+    bool isPrime = sieve.isPrime(x);
+
+    if(isPrime)
+        cout << x << " is prime\n";
+    else
+        cout << x << " is not prime\n";
+
+    return 0;
+}
