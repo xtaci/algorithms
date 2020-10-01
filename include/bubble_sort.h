4b2f76d82b1a2e635e281d9c80e39fbdffb416d1
Pranay711 <71118731+Pranay711@users.noreply.github.com>
2020-10-01 15:27:31 +0530

    Update bubble_sort.h
    @wycg1984 @Samana @blackball @pierredavidbelanger  changes: bubble sort algorithm in python language.

--- include/bubble_sort.h
+++ include/bubble_sort.h
@@ -19,33 +19,31 @@
  *
  * http://en.wikipedia.org/wiki/Bubble_sort
  ******************************************************************************/
+# Python program for implementation of Bubble Sort 
 
-#ifndef _BUBBLE_SORT_H_
-#define _BUBBLE_SORT_H_
+def bubbleSort(arr): 
+	n = len(arr) 
 
-#include <assert.h>
-#include <generic.h>
+	# Traverse through all array elements 
+	for i in range(n): 
 
-namespace alg {
-	template<typename T>
-		static void	BubbleSort(T list[], int start, int end){
-			int i;
-			bool swapped;
+		# Last i elements are already in place 
+		for j in range(0, n-i-1): 
 
-			assert(start < end);
+			# traverse the array from 0 to n-i-1 
+			# Swap if the element found is greater 
+			# than the next element 
+			if arr[j] > arr[j+1] : 
+				arr[j], arr[j+1] = arr[j+1], arr[j] 
 
-			do {
-				swapped = false;
-				for(i = start+1; i <= end; i++) {
-					if(list[i-1] > list[i]) {
-						// swap them and remember something changed
-						swap(list[i-1], list[i]);
-						swapped = true;
-					}
-				}
-			} while(swapped);
-		}
-}
+# Driver code to test above 
+arr = [64, 34, 25, 12, 22, 11, 90] 
+
+bubbleSort(arr) 
+
+print ("Sorted array is:") 
+for i in range(len(arr)): 
+	print ("%d" %arr[i]), 
 
 
 #endif // _BUBBLE_SORT_H_
