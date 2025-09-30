c6dccd3437474bfd1d6aec28890fa7188b828300
Siddhant Singh <singhsiddhantakshat@gmail.com>
2025-09-29 20:01:57 -1000

    chore: (more reorganization.)

---
+++ include/siddhant's-algorithm.h
@@ -0,0 +1,21 @@
+/********************************
+
+What am I trying to do? 
+-_____ \
+
+        |
+        |
+        v
+        Assignment Requirements for the Algorithms project.
+1. I need to make something that shows a nice DEMO. Demonstration. -> The way I can do this is by doing the following:
+	1. I can start.
+		2. I can try to figure out how to properly do dependency management (*Decompress a Directed Acyclic Graph -> DAG RFC. With Autocomplete.)
+
+
+
+*/
+
+#ifndef _SIDDHANT_SINGH___
+
+
+#endif //
---
+++ msvc/me_v_solver.h
@@ -0,0 +1,19 @@
+/* 
+Dependency Management is likely the future.
+|
+
+\
+        \ 
+
+
+            |
+
+            v
+            
+
+            The reason; (We - I, etc.) say this is because proper surveys' can make OR break a companies routine etc. There's a lot of beauty to be able to be seen within this System.
+Systems assignment for COE 379L (P.I. -> Marcus.)
+
+
+*/
+
