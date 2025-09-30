7670ff4887318df845f9b9aec6e0beafdb22c997
Siddhant Singh <singhsiddhantakshat@gmail.com>
2025-09-29 20:09:50 -1000

    chore(re-org for proj.): Siddhant did the work; But I like the Title: Santa's Little Helper.
    Hi! Ok. Locked in Again (I'm on a plane and there's so Much Data input which is Valid&N_øP simultaneously.

--- include/siddhant's-algorithm.h
+++ include/siddhant's-algorithm.h
@@ -11,11 +11,26 @@ What am I trying to do?
 	1. I can start.
 		2. I can try to figure out how to properly do dependency management (*Decompress a Directed Acyclic Graph -> DAG RFC. With Autocomplete.)
 
-
-
+Today, I will be making the world's first. (Probably not first)
+    -> Realtime Dependency manamgment/cross-platform/Nop(Not an Operation.)
 */
 
 #ifndef _SIDDHANT_SINGH___
+#include <stdint.h>
+#include <stdlib.h>
+
+namespace alg {
+    /*
+        M.J. plays in the background. (this is a Micheal Jackson Song.)
+    */
+    template    <typename T=(***?***)>
+        class V {
+            private:
+            public:
+
+        };
 
 
-#endif //
+};
+
+#endif //-> although I don't know magic. I can learn it. (Santa's Little Helper.)
